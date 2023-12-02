9c4710e113e4530f0161f32396f4a0499070f072
Kru301019 <krujoshi3010@gmail.com>
2023-12-02 06:29:11 +0000

    lab 8 update

---
+++ Database princi/8.lab_eight.sql
@@ -0,0 +1,53 @@
+SELECT
+    a.actor_id AS actor_id,
+    a.first_name AS actor_first_name,
+    a.last_name AS actor_last_name
+FROM
+    actor AS a    
+WHERE
+    a.first_name ILIKE 'scarlett%';    
+
+
+SELECT 
+    last_name,COUNT(last_name) AS unique_last_name 
+FROM 
+    actor
+GROUP BY 
+    last_name
+ORDER BY 
+    last_name;
+
+
+SELECT last_name AS actor_with_unique_lastName
+FROM actor
+GROUP BY last_name
+HAVING COUNT(*)=1
+ORDER BY last_name;
+
+SELECT 
+    a.first_name,
+    a.last_name,
+    COUNT(*)
+
+FROM actor AS a
+
+GROUP BY 
+    a.first_name,
+    a.last_name;
+
+
+SELECT 
+    a.first_name AS actor_first_name, 
+    a.last_name AS actor_last_name,
+    COUNT(*) AS appearance_count
+FROM 
+    actor AS a
+JOIN 
+    movie_actor  USING (actor_id)
+GROUP BY 
+    a.first_name,
+    a.last_name
+ORDER BY 
+    appearance_count DESC
+LIMIT 1;
+    
